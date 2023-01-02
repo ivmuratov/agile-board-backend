7557fd94e593e5b640383d0586f6d0ace621d609
Muratov Ivan <ivanmuratov121096@gmail.com>
2023-01-02 15:43:57 +0700

    prettier config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const PORT = process.env.PORT || 8080;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(8080);
+  const logger = new Logger(bootstrap.name);
+  await app.listen(PORT, () => logger.log(`server started on ${PORT} port`));
 }
 bootstrap();
